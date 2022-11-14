1ed771f306707702048c804726a1752aba462ad3
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-11-14 15:51:31 +0200

    Add models, migrate changes

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255, unique=True)
+    created_date = models.DateField(auto_now_add=True)
+    deadline = models.DateField(null=True, blank=True)
+    status = models.BooleanField(default=False)
+    tags = models.ManyToManyField(to=Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["status", "created_date"]
+
+    def __str__(self):
+        return f"{self.content} ({self.status}, {self.tags}, {self.created_date})"
