490fdd25ede8e98b5c93e48dbf52e8c8cf73a2b9
Lina Samoilenko <salina.amoylenko@gmail.com>
2022-11-14 15:52:44 +0200

    Add views

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,62 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect
+from django.urls import reverse_lazy
+from django.views import generic, View
 
-# Create your views here.
+from tasks.forms import TaskForm, TagForm
+from tasks.models import Task, Tag
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    paginate_by = 5
+    queryset = Task.objects.all().prefetch_related("tags")
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    form_class = TaskForm
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-delete")
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    paginate_by = 5
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    form_class = TagForm
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class ChangeTaskStatusView(View):
+    def post(self, request, *args, **kwargs):
+        obj = get_object_or_404(Task, pk=self.kwargs['pk'])
+
+        obj.status = not obj.status
+        obj.save()
+        return redirect(reverse_lazy("tasks:task-list"))
